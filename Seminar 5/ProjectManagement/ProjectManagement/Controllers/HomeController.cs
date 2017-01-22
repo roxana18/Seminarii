75430d70ef2fd9fbf239418336e6e14a5ff13986
Tirlea Elena Roxana <tirlea.roxana94@yahoo.com>
2017-01-22 11:40:30 +0200

    Seminar 5

---
+++ Seminar 5/ProjectManagement/ProjectManagement/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectManagement.Models;
+
+namespace ProjectManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Author = "Roxana";
+            var model = new AboutModel();
+            model.Message = "Exercise 7";
+            model.Author = "Roxana Tirlea";
+            return View(model);
+        
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
