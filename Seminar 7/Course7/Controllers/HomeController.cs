810525e709617fecc8730429cb5ef7ce0994e201
Tirlea Elena Roxana <tirlea.roxana94@yahoo.com>
2017-01-22 11:43:37 +0200

    Seminar 7

---
+++ Seminar 7/Course7/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Course7.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [ActionName("authorize")]
+        [Authorize(Roles ="Admin")]
+        public ActionResult ThisMethodNeedsAuthorization()
+        {
+            return Content("Hello from ThisMethodNeedsAuthorization action");
+        }
+
+        [HttpGet]
+        public ActionResult VerbsTest()
+        {
+            return Content("Hello");
+        }
+       
+       [NonAction]
+        public ActionResult Calc()
+        {
+            return Content(" Calc Action");
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ActionName("about")]
+        public ActionResult SomeMethodName()
+        {
+            return View("SomeMethodName");
+        }
+    }
+}
