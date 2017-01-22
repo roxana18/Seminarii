d38560a7e3dbeacdc0e2b6e39f4e02cbaf0d7da6
Tirlea Elena Roxana <tirlea.roxana94@yahoo.com>
2017-01-22 12:02:26 +0200

    Seminar 9

---
+++ Seminar 9/Course8/Models/ProjectEvaluation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Course8.Models
+{
+    [Bind(Exclude = "Country")]
+    public class ProjectEvaluation
+    {
+    
+        public string Name
+        { 
+            get; set;
+        }
+        public string City
+        {
+            get; set;
+        }
+        public string Country
+        {
+            get; set;
+        }
+        [Range(1,10)]
+        [Required]
+        public double Rating
+        {
+            get; set;
+        }
+
+        public int Id
+        {
+            get; set;
+        }
+        [StringLength(100)]
+        public string Description
+        {
+            get; set;
+        }
+    }
+}
