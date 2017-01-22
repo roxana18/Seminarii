6aafe5f86f84fc930bd1e8a803e81f845a112717
Tirlea Elena Roxana <tirlea.roxana94@yahoo.com>
2017-01-22 11:35:42 +0200

    Seminar 2

---
+++ Seminar2/UbbCourse1/Classes Tutorial/Classes Tutorial/Mercedes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Tutorial
+{
+    //Exercise 9  top level classes cannot be protected, they must be inner classes
+  internal class Mercedes : AbstractCar
+    {
+        public string description;
+        public int price;
+        private string _description;
+        private int _price;
+
+        public Mercedes()
+        {
+            description = "E Class";
+            price = 67000;
+        }
+        //If the properties are protected they are inaccessible for Program.cs
+
+        internal string Description
+        {
+            get { return _description; }
+            set { _description = value + "property Description"; }
+        }
+
+        internal int Price
+        {
+            get { return _price; }
+            set { _price = value + 50000; }
+        }
+    }
+}
---
+++ Seminar2/UbbCourse1/Classes Tutorial/Classes Tutorial/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercise 4
+            Mercedes mercedes = new Mercedes();
+            //AbstractCar aCar = new AbstractCar(); =>abstract classes cannot be instantiated
+
+            //Exercise 6
+            ICar car = new Mercedes();
+            AbstractCar car2 = new Mercedes();
+
+            //Exercise 7
+            Console.WriteLine("Mercedes is " + mercedes.description);
+            Console.WriteLine("Mercedes costs " + mercedes.price);
+
+            //Exercise 8 
+            mercedes.Description = "E Class ";
+            mercedes.Price = 50;
+            Console.WriteLine(mercedes.Description);
+            Console.WriteLine(mercedes.Price);
+
+            //Exercise 5
+            EngineStruct engine = new EngineStruct();
+
+            //Exercise 1
+            Console.WriteLine("This is my first C# project!");
+            Console.ReadKey();
+        }
+    }
+
+    struct EngineStruct
+    {
+    }
+}
